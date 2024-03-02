80e16d274e4e58f84e846375e8e16066ea13f085
Rui Fan <ruifan@indiana.edu>
2024-03-01 21:49:12 -0800

    add dropdown arrow icon

--- src/app/components/Dropdown.tsx
+++ src/app/components/Dropdown.tsx
@@ -3,13 +3,27 @@
 import Link from 'next/link';
 import { Menu, Transition } from '@headlessui/react';
 import { Fragment, useEffect, useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 export default function Dropdown() {
   return (
     <div className='text-right'>
       <Menu as='div' className='relative inline-block text-left'>
         <div>
-          <Menu.Button>Create</Menu.Button>
+          <Menu.Button>
+            {({ open }) => (
+              <>
+                Create
+                <FontAwesomeIcon
+                  className='-mr-1 ml-2'
+                  icon={faChevronUp}
+                  rotation={open ? undefined : 180}
+                  size='sm'
+                />
+              </>
+            )}
+          </Menu.Button>
         </div>
         <Transition
           as={Fragment}
@@ -24,50 +38,54 @@ export default function Dropdown() {
             <div className='px-1 py-1 '>
               <Menu.Item>
                 {({ active }) => (
-                  <button
-                    className={`${
-                      active
-                        ? 'bg-neutral-200 text-neutral-600'
-                        : 'text-gray-900'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {active ? (
-                      <EditActiveIcon
-                        className='mr-2 h-5 w-5'
-                        aria-hidden='true'
-                      />
-                    ) : (
-                      <EditInactiveIcon
-                        className='mr-2 h-5 w-5'
-                        aria-hidden='true'
-                      />
-                    )}
-                    <Link href='/create/compose'>Compose</Link>
-                  </button>
+                  <Link href='/create/compose'>
+                    <button
+                      className={`${
+                        active
+                          ? 'bg-neutral-200 text-neutral-600'
+                          : 'text-gray-900'
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      {active ? (
+                        <EditActiveIcon
+                          className='mr-2 h-5 w-5'
+                          aria-hidden='true'
+                        />
+                      ) : (
+                        <EditInactiveIcon
+                          className='mr-2 h-5 w-5'
+                          aria-hidden='true'
+                        />
+                      )}
+                      Compose
+                    </button>
+                  </Link>
                 )}
               </Menu.Item>
               <Menu.Item>
                 {({ active }) => (
-                  <button
-                    className={`${
-                      active
-                        ? 'bg-neutral-200 text-neutral-600'
-                        : 'text-gray-900'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {active ? (
-                      <DuplicateActiveIcon
-                        className='mr-2 h-5 w-5'
-                        aria-hidden='true'
-                      />
-                    ) : (
-                      <DuplicateInactiveIcon
-                        className='mr-2 h-5 w-5'
-                        aria-hidden='true'
-                      />
-                    )}
-                    <Link href='/create/rewrite'>Rewrite</Link>
-                  </button>
+                  <Link href='/create/rewrite'>
+                    <button
+                      className={`${
+                        active
+                          ? 'bg-neutral-200 text-neutral-600'
+                          : 'text-gray-900'
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      {active ? (
+                        <DuplicateActiveIcon
+                          className='mr-2 h-5 w-5'
+                          aria-hidden='true'
+                        />
+                      ) : (
+                        <DuplicateInactiveIcon
+                          className='mr-2 h-5 w-5'
+                          aria-hidden='true'
+                        />
+                      )}
+                      Rewrite
+                    </button>
+                  </Link>
                 )}
               </Menu.Item>
             </div>
